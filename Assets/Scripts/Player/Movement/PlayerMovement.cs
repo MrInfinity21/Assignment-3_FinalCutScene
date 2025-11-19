8e651349d03b95ece94780f52bedcb2f668aed96
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-11-18 18:21:30 -0700

    Added Run State

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -5,24 +5,31 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private InputSystem_Actions _inputActions;
-    private CharacterController _characterControl;
+    private CharacterController _characterController;
 
     private Vector2 _moveInput;
 
-    [Header("Movement Values")]
+    [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f; // Speed for Running
 
     [Header("Animator")]
     [SerializeField] private Animator animator;
 
+    private bool _isRunning = false;
+
     private void Awake()
     {
-        _characterControl = GetComponent<CharacterController>();
+        _characterController = GetComponent<CharacterController>();
         _inputActions = new InputSystem_Actions();
 
-        // Listen for movement input
+        // Movement input
         _inputActions.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
         _inputActions.Player.Move.canceled += ctx => _moveInput = Vector2.zero;
+
+        // Sprint input
+        _inputActions.Player.Sprint.performed += ctx => _isRunning = true;
+        _inputActions.Player.Sprint.canceled += ctx => _isRunning = false;
     }
 
     private void OnEnable() => _inputActions.Enable();
@@ -36,23 +43,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void MoveCharacter()
     {
-        Vector3 moveDirection =
-            transform.right * _moveInput.x +
-            transform.forward * _moveInput.y;
-
-        _characterControl.Move(moveDirection * walkSpeed * Time.deltaTime);
+        float currentSpeed = (_isRunning && _moveInput.sqrMagnitude > 0.01f) ? runSpeed : walkSpeed;
+        Vector3 moveDir = transform.right * _moveInput.x + transform.forward * _moveInput.y;
+        _characterController.Move(moveDir * currentSpeed * Time.deltaTime);
     }
 
     private void UpdateAnimation()
     {
-        // Update directional movement
-        animator.SetFloat("Horizontal", _moveInput.x);
-        animator.SetFloat("Vertical", _moveInput.y);
+        // Update walking Blend Tree velocities
+        animator.SetFloat("Velocity X", _moveInput.x);
+        animator.SetFloat("Velocity Z", _moveInput.y);
 
-        // Calculate speed magnitude (0 = idle, 1 = full movement)
-        float speed = _moveInput.magnitude;
-
-        // Update speed for transitions
-        animator.SetFloat("Speed", speed);
+        // Update booleans
+        animator.SetBool("isWalking", _moveInput.sqrMagnitude > 0.01f && !_isRunning);
+        animator.SetBool("isRunning", _isRunning && _moveInput.sqrMagnitude > 0.01f);
     }
 }
